37776fcb2f94a8850710871ef2ff0ff2ba7e81d2
Marc Heinimann <marckevin.heinimann@uzh.ch>
2024-03-24 13:41:21 +0100

    changed login functionality #96

--- src/main/java/ch/uzh/ifi/hase/soprafs24/service/UserService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/UserService.java
@@ -14,9 +14,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.util.*;
-import java.io.File;
-import java.nio.file.Files;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -24,6 +23,7 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.awt.geom.Path2D;
 import java.util.List;
+import java.io.File;
 
 /**
  * User Service
@@ -94,16 +94,10 @@ public class UserService {
   public void saveProfilePicture(Long userId, String token, MultipartFile imageFile) throws IOException {
     User user = userRepository.findById(userId).orElseThrow(() ->
             new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
-
-    /*if (!Objects.equals(user.getToken(), token)) {
-      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-    }*/
-
+    // compare tokens
     byte[] imageData = imageFile.getBytes();
-    System.out.println(Arrays.toString(imageData));
     Image profileImage = new Image();
     profileImage.setProfilePicture(imageData);
-
     user.setProfileImage(profileImage);
     userRepository.save(user);
     userRepository.flush();
@@ -112,48 +106,44 @@ public class UserService {
   public byte[] getProfilePicture(Long userId, String token) {
     User user = userRepository.findById(userId).orElseThrow(() ->
             new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
-
-    /*if (!Objects.equals(user.getToken(), token)) {
-      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-    }*/
-
-    Image profileImage = user.getProfileImage();
-    if (profileImage != null) {
-      return profileImage.getProfilePicture();
-    } else {
-      // Return default image or throw an exception, depending on your requirements
-      return generateDefaultImage();
-    }
+    // compare tokens
+    /*Image profileImage = user.getProfileImage();
+    return profileImage.getProfilePicture();*/
+    byte[] imageData = generateDefaultImage("Masd");
+    saveGeneratedImage(imageData, "default_image.jpg");
+    return imageData;
   }
 
 
-  private byte[] generateDefaultImage() {
-    int width = 100;
-    int height = 100;
+  private byte[] generateDefaultImage(String userName) {
+    int width = 1000;
+    int height = 1000;
+    String text = "--";
+    if (userName.length() > 1) {
+      text = userName.substring(0,2).toUpperCase();
+    }
+
     BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
     Graphics2D g2d = image.createGraphics();
-
+    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
     // random background colour
     Random random = new Random();
     Color bgColor = new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
 
     g2d.setColor(bgColor);
     g2d.fillRect(0, 0, width, height);
-
-    // Draw a yellow star in the middle of the image
-    g2d.setColor(Color.YELLOW);
-
-    int[] xPoints = {50, 55, 70, 57, 61, 50, 39, 43, 30, 45};
-    int[] yPoints = {15, 35, 35, 45, 60, 50, 60, 45, 35, 35};
-
-    Path2D star = new Path2D.Double();
-    star.moveTo(xPoints[0], yPoints[0]);
-    for (int i = 1; i < xPoints.length; i++) {
-      star.lineTo(xPoints[i], yPoints[i]);
-    }
-    star.closePath();
-    g2d.fill(star);
+    Font font = new Font("Arial", Font.BOLD, 300);
+    g2d.setFont(font);
+    g2d.setColor(Color.WHITE);
+    FontMetrics fm = g2d.getFontMetrics(); // Get font metrics for accurate text placement
+    int textWidth = fm.stringWidth(text);
+    int textHeight = fm.getHeight();
+    int x = (width - textWidth) / 2; // Center the text horizontally
+    int y = (height - textHeight) / 2 + fm.getAscent(); // Center the text vertically
+    g2d.drawString(text, x, y);
     g2d.dispose();
+
 
     // Convert the BufferedImage to byte array
     try {
@@ -166,6 +156,18 @@ public class UserService {
       return null;
     }
   }
+  private void saveGeneratedImage(byte[] imageData, String fileName) {
+    try {
+      // Create a new file with the specified file name
+      File file = new File(fileName);
 
+      // Write the image data to the file
+      ImageIO.write(ImageIO.read(new ByteArrayInputStream(imageData)), "jpg", file);
+
+      System.out.println("Image saved as: " + file.getAbsolutePath());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
 }
