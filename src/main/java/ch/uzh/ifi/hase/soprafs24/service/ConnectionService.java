c35f15b45b6a749c7d9562da10299deb89fccb85
Gabriel Stegmaier <gabriel.stegmaier@bluewin.ch>
2024-04-02 22:28:18 +0200

    Changed rest mapping of the start point search #119

--- src/main/java/ch/uzh/ifi/hase/soprafs24/service/ConnectionService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/ConnectionService.java
@@ -43,9 +43,12 @@ public class ConnectionService {
         JSONObject jsonStation = jsonArray.getJSONObject(i);
 
         Station retStation = new Station();
-        retStation.setStationCode(jsonStation.getString("id"));
-        retStation.setStationName(jsonStation.getString("name"));
-        stations.add(retStation);
+        Object nullableId = jsonStation.get("id");
+        if (nullableId instanceof String) {
+          retStation.setStationCode(jsonStation.getString("id"));
+          retStation.setStationName(jsonStation.getString("name"));
+          stations.add(retStation);
+        }
       }
       return stations;
     } catch(IOException | InterruptedException apiException) {
@@ -54,34 +57,39 @@ public class ConnectionService {
   }
 
   public static List<Station> getLocationsCoord(String x, String y) {
-  try {
-    // Http request to transport.opendata
-    HttpClient client = HttpClient.newHttpClient();
-    HttpRequest request = HttpRequest.newBuilder()
+    try {
+      // Http request to transport.opendata
+      HttpClient client = HttpClient.newHttpClient();
+      HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create(String.format("http://transport.opendata.ch/v1/locations?x=%s&y=%s", x, y)))
             .version(HttpClient.Version.HTTP_2)
             .GET()
             .build();
-    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+      HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
     // parse string to JSON object & create empty list of stations
-    JSONObject obj = new JSONObject(response.body());
-    JSONArray jsonArray = obj.getJSONArray("stations");
-    List<Station> stations = new ArrayList<>();
+      JSONObject obj = new JSONObject(response.body());
+      JSONArray jsonArray = obj.getJSONArray("stations");
+      List<Station> stations = new ArrayList<>();
 
     // extract name and id of stations and add them to the list of stations
-    for (int i = 0; i < jsonArray.length(); i++) {
-      JSONObject jsonStation = jsonArray.getJSONObject(i);
+      for (int i = 0; i < jsonArray.length(); i++) {
+        JSONObject jsonStation = jsonArray.getJSONObject(i);
 
-      Station retStation = new Station();
-      retStation.setStationCode(jsonStation.getString("id"));
-      retStation.setStationName(jsonStation.getString("name"));
-      stations.add(retStation);
+        Station retStation = new Station();
+        Object nullableId = jsonStation.get("id");
+        if (nullableId instanceof String) {
+          retStation.setStationCode(jsonStation.getString("id"));
+          retStation.setStationName(jsonStation.getString("name"));
+          stations.add(retStation);
+        }
+      }
+      return stations;
+    } catch(IOException | InterruptedException apiException) {
+      throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "There was an error during API request to the external transport api");
     }
-    return stations;
-  } catch(IOException | InterruptedException apiException) {
-    throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "There was an error during API request to the external transport api");
   }
-}
+
+
 
 }
